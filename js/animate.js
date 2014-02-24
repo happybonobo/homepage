ca7125c48274b1cff9a50f4151531f7be6d5b785
Chris Antaki <ChrisAntaki@gmail.com>
2014-02-24 03:47:19 -0800

    New styles

--- js/animate.js
+++ js/animate.js
@@ -1,17 +1,10 @@
-var body = document.getElementsByTagName('body')[0];
+var p = document.getElementsByTagName('p')[0];
 
-if (!navigator.userAgent.match(/Firefox/)) {
-    body.style.backgroundSize = 'inherit';
+degrees = 0;
+function rotate() {
+    degrees += 360;
+    p.style.transform = "rotate("+degrees+"deg)";
+    p.style['-webkit-transform'] = "rotate("+degrees+"deg)";
 }
 
-setInterval(loop, 250);
-
-function loop() {
-    var x = getRandomNumber(),
-        y = getRandomNumber();
-    body.style.backgroundPosition = x + 'px ' + y + 'px';
-}
-
-function getRandomNumber() {
-    return Math.round(Math.random() * 100);
-}
+setInterval(rotate, 4000);
